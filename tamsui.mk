dfc0ea1335b57b2dfed65c2fd49376efe14eaa7b
Joey Riches <josephriches@gmail.com>
2013-12-30 09:19:57 +0000

    fix derp

--- tamsui.mk
+++ tamsui.mk
@@ -71,7 +71,7 @@ PRODUCT_PACKAGES += \
     audio_policy.msm7x27a \
     audio.primary.msm7x27a \
     audio_policy.conf \
-    libaudioutilsqcom
+    libaudioutils
 
 PRODUCT_COPY_FILES += \
     device/sony/tamsui-common/config/media_profiles.xml:system/etc/media_profiles.xml \
