e2151f837bbba4b7f41b8401f894b8150bebecc2
Joey Riches <josephriches@gmail.com>
2014-03-11 10:01:54 +0000

    tamsui: build chromium webview
    with updated kernel & ro.webview.gralloc_unbind enabled, i'm confident we can get acceptable performance. 
    
    We shouldn't be looking backwards and using obsolete code, and is generally bad practice.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -115,7 +115,6 @@ BOARD_USES_LEGACY_RIL := true
 BOARD_RIL_CLASS := ../../../device/sony/tamsui-common/ril/
 
 # Web Rendering
-PRODUCT_PREBUILT_WEBVIEWCHROMIUM := true
 TARGET_FORCE_CPU_UPLOAD := true
 ENABLE_WEBGL := true
 
