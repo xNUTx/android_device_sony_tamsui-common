05741abc5a86e872f480acf9047d0cc0bc467042
Joey Riches <josephriches@gmail.com>
2014-04-01 12:39:39 +0100

    libaudio: build audio blobs as .sony not .msm7x27a
    build audio.primary and audio_policy as sony to match stock firmware.
    When updating the audio HAL I made the mistake of renaming the blobs as the chipsest (msm7x27a)

--- libaudio/Android.mk
+++ libaudio/Android.mk
@@ -44,7 +44,7 @@ LOCAL_STATIC_LIBRARIES := \
     libmedia_helper \
     libaudiohw_legacy
 
-LOCAL_MODULE := audio.primary.msm7x27a
+LOCAL_MODULE := audio.primary.sony
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE_TAGS := optional
 
@@ -79,7 +79,7 @@ LOCAL_STATIC_LIBRARIES := \
     libmedia_helper \
     libaudiopolicy_legacy
 
-LOCAL_MODULE := audio_policy.msm7x27a
+LOCAL_MODULE := audio_policy.sony
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE_TAGS := optional
 
