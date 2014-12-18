2e341481dbbd135da8b1d347685f412a754b9130
xNUTx <nut@fun-industries.nl>
2014-12-19 00:17:44 +0100

    TWRP Build configuration

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -126,3 +126,25 @@ ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
 ADDITIONAL_DEFAULT_PROPERTIES := ro.adb.secure=0	\
 	ro.secure=0
 endif
+
+# TWRP flags
+DEVICE_RESOLUTION := 480x854
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_HAS_NO_RECOVERY_PARTITION := true
+TW_FLASH_FROM_STORAGE := true
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_DEFAULT_EXTERNAL_STORAGE := true
+# TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_JB_CRYPTO := true
+TW_CRYPTO_FS_TYPE := "ext4"
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/platform/msm_sdcc.1/by-name/Userdata"
+TW_CRYPTO_MNT_POINT := "/data"
+TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,barrier=1,noauto_da_alloc,discard"
+TW_CRYPTO_FS_FLAGS := "0x00000406"
+TW_CRYPTO_KEY_LOC := "footer"
+TW_INCLUDE_FUSE_EXFAT := true
+# TW_BOARD_CUSTOM_GRAPHICS := ../../../device/sony/blue-common/recovery/twrpgraphics.c
+TW_MAX_BRIGHTNESS := 255
+TW_NO_USB_STORAGE := true
