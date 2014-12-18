21cdf063b4e78ed83edb6eda1b050600f2324c09
xNUTx <nut@fun-industries.nl>
2014-12-19 00:20:00 +0100

    TWRP Build configuration

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -145,6 +145,7 @@ TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,barrier=1,noauto_da_alloc,discard"
 TW_CRYPTO_FS_FLAGS := "0x00000406"
 TW_CRYPTO_KEY_LOC := "footer"
 TW_INCLUDE_FUSE_EXFAT := true
-# TW_BOARD_CUSTOM_GRAPHICS := ../../../device/sony/blue-common/recovery/twrpgraphics.c
 TW_MAX_BRIGHTNESS := 255
 TW_NO_USB_STORAGE := true
+# TW_BOARD_CUSTOM_GRAPHICS := ../../../device/sony/blue-common/recovery/twrpgraphics.c
+# TW_BRIGHTNESS_PATH := /sys/devices/i2c-0/0-0036/leds/lm3533-lcd-bl/brightness
